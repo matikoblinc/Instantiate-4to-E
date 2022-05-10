88f1e11f668bf50f99f8b97444ec61bdae1cb2cc
matikoblinc <78440014+matikoblinc@users.noreply.github.com>
2022-05-10 13:52:58 -0300

    UI text input

---
+++ Assets/DataInputManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+
+    public Text txtUserName;
+
+    public void ShowGreetings ()
+    {
+        Debug.Log("hola " + txtUserName.text);
+    }
+
+}
